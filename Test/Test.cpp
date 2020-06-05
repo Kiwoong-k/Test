8eb39179e3c3b0cbcc8dc8806a1130192d73bab6
기웅 김 <rlarldnd99@naver.com>
2020-06-05 11:48:05 +0900

    first

---
+++ Test/Test.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+	int arr[7] = { 1,4,3,2,5,7,6 };
+	int *front = &arr[0];
+	int *back  = &arr[6];
+	for (int a = 0; a < 3; a++)
+	{
+		int num = *(front + a);//1 4 3
+		*(front + a) = *(back - a);//1 4 3 = 
+		*(back - a) = num;
+	}
+
+	for (int i = 0; i < 7; i++)
+	{
+		cout << arr[i] << " ";
+	}
+}
+/*
+	for (int a = 0; a < 3; a++)
+	{
+		int num = *front;
+		*front = *back;
+		*back = num;
+		back--;
+		front++;
+
+	}*/
